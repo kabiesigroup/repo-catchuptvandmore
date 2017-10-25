c07ae9389a5c546f2c2ab04446e89eda6802d0cd
SylvainCecchetto <cecchetto.sylvain@me.com>
2017-10-25 10:39:32 +0200

    First commit

---
+++ crontab_line.sh
@@ -0,0 +1,1 @@
+0 1 * * * /var/www/my_webapp__2/update_repo.sh
---
+++ update_repo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Update Krypton master version
+cd github
+wget https://github.com/SylvainCecchetto/plugin.video.catchuptvandmore/archive/master.zip -O master.zip
+unzip -o master.zip
+../create_repository.py --datadir=../www/krypton/release plugin.video.catchuptvandmore-master
+
+# Update Jarvis master version
+sed -i 's/<import addon="xbmc.python" version="2.25.0"\/>/<import addon="xbmc.python" version="2.24.0"\/>/' plugin.video.catchuptvandmore-master/addon.xml
+../create_repository.py --datadir=../www/jarvis/release plugin.video.catchuptvandmore-master
+
+# Update Krypton dev version
+cd github
+wget https://github.com/SylvainCecchetto/plugin.video.catchuptvandmore/archive/dev.zip -O dev.zip
+unzip -o dev.zip
+../create_repository.py --datadir=../www/krypton/beta plugin.video.catchuptvandmore-dev
+
+# Update Jarvis dev version
+sed -i 's/<import addon="xbmc.python" version="2.25.0"\/>/<import addon="xbmc.python" version="2.24.0"\/>/' plugin.video.catchuptvandmore-dev/addon.xml
+../create_repository.py --datadir=../www/jarvis/beta plugin.video.catchuptvandmore-dev
+
+
+
+
+
+
